f09965233c4175dbc4b5bd1bc30260760769e88c
Paul Rector <paulre@skyward.com>
2019-09-13 15:22:01 -0500

    Add home page logo

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <img alt="Vue logo" src=".\assets\cub-scout-ranks.jpg">
+    <HelloWorld msg="Cub Scout Pack 299"/>
   </div>
 </template>
 
