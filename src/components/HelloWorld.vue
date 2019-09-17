8c31a3bb380c48d7eae262e2b27a02feff43e972
Paul Rector <epunman@gmail.com>
2019-09-17 15:35:21 -0500

    Edited links

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,30 +2,32 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
+      Chartered by <a href="http://stevenspointumc.org/" target="_blank" rel="noopener">Saint Paul's United Methodist Church</a> 600 Wilshire Blvd Stevens Point, WI
     </p>
-    <h3>Installed CLI Plugins</h3>
+    <h3>Events</h3>
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
+      <li><a href="https://www.scoutbook.com/mobile/dashboard/calendar/event.asp?EventID=2439006" target="_blank" rel="noopener">Pack Meeting 9/17/19 6:30pm</a></li>
+      <li><a href="https://www.scoutbook.com/mobile/dashboard/calendar/event.asp?EventID=2439203" target="_blank" rel="noopener">Webelos Fall Adventure 9/20/19-9/22/19</a></li>
+      <li><a href="https://docs.google.com/document/d/1OXD9YnCUTupCrfO7rTAY_mS9K392yU1RqawAxZm4oVU/edit?usp=sharing" target="_blank" rel="noopener">SuperTrip to Mall of America Aquarium</a></li>
     </ul>
-    <h3>Essential Links</h3>
+    <h3>Formalities</h3>
     <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
+      <li><a href="https://docs.google.com/document/d/1C1AkoLoxtr2BhEoArkRUiCi8oPV9QwKEyYWBqYQoQPg/edit?usp=sharing" target="_blank" rel="noopener">By-Laws</a></li>
+      <!-- <li><a href="https://google.com" target="_blank" rel="noopener">Dues</a></li> -->
+      <li><a href="https://www.scouting.org/programs/cub-scouts/cub-scout-uniform/" target="_blank" rel="noopener">Uniforms</a></li>
     </ul>
-    <h3>Ecosystem</h3>
+    <h3>Tools</h3>
     <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+      <li><a href="https://docs.google.com/document/d/17Dodjr9mJcCv2Uow61wxoqbGCRBnL8BMTcPh-rWhVO0/edit?usp=sharing" target="_blank" rel="noopener">Youth Protection Training</a></li>
+      <li><a href="https://www.scoutshop.org/" target="_blank" rel="noopener">Online Scout Shop</a> (Galaxy Hobby in Plover or the Scout Office in Weston are other sources)</li>
+      <li><a href="https://docs.google.com/document/d/1-9RZQRIt9zYCuC0VQMO7eOVBTnQGsh_USJ5GobRvVk4/edit?usp=sharing" target="_blank" rel="noopener">HuTerra Fundraiser</a></li>
+      <!-- <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">Scouter Terminology</a></li> -->
+      <li><a href="https://paypal.me/pack299?locale.x=en_US" target="_blank" rel="noopener">Pack Paypal</a> (Please send enough to cover the paypal <a href="https://salecalc.com/paypal" target="_blank" rel="noopener">Fee</a>)</li>
+    </ul>
+    <h3>Communication</h3>
+    <ul>
+      <li><a href="https://www.scoutbook.com/" target="_blank" rel="noopener">Scoutbook</a></li>
+      <li><a href="https://docs.google.com/document/d/1vVM2PZomGcLZR2ZSEDbMJfIe1pkDyAlA3yeLEiAqqt8/edit?usp=sharing" target="_blank" rel="noopener">RSS Feed</a></li>
     </ul>
   </div>
 </template>
