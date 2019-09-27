fa99d1f9f69192f2cb4fb3286c2427f57eef693f
Paul Rector <paulre@skyward.com>
2019-09-27 16:29:33 -0500

    Added bootstrap-vue and axios

--- src/main.js
+++ src/main.js
@@ -1,13 +1,16 @@
 import Vue from 'vue'
+import BootstrapVue from 'bootstrap-vue'
 import App from './App.vue'
 import Router from 'vue-router'
 import routes from './routes';
-import 'bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 Vue.config.productionTip = false
 
 Vue.use(Router)
+Vue.use(BootstrapVue)
 
 const router = new Router({
   mode: 'history',
