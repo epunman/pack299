3a4ec31f93a46a029bbadc44b2d0f2a627110491
Paul Rector <paulre@skyward.com>
2019-09-27 12:34:05 -0500

    Added fallback and 404 redirect

---
+++ src/components/NotFound.vue
@@ -0,0 +1,13 @@
+// NotFound.vue
+
+<template>
+    <div>
+        <h3>The Page You Requested Does Not Exist</h3>
+        <a href>
+    </div>
+</template>
+
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
