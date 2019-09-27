af7e78e71bbffaa20b03047062ad3fbcff7783fd
Paul Rector <paulre@skyward.com>
2019-09-27 14:45:27 -0500

    More server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const history = require('connect-history-api-fallback')
 const express = require('express');
 const path = require('path');
-// const serveStatic = require('serve-static');
+const serveStatic = require('serve-static');
 const app = express();
 // const staticFileMiddleware = express.static(path.join(__dirname))
 
@@ -11,8 +11,8 @@ app.use(history({
     verbose: true
 }))
 // app.use(staticFileMiddleware)
-// app.use(serveStatic(__dirname + "/dist"));
-app.use(__dirname + "/dist");
+app.use(serveStatic(__dirname + "/dist"));
+//app.use(__dirname + "/dist");
 
 app.listen(process.env.PORT || 5000, function(){
     console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
