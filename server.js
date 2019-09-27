4ff81502a6fb76459df2cddb83f0e7a08512e5d2
Paul Rector <paulre@skyward.com>
2019-09-27 14:11:58 -0500

    More edits to server.js

--- server.js
+++ server.js
@@ -2,17 +2,17 @@
 const history = require('connect-history-api-fallback')
 const express = require('express');
 const path = require('path');
-// var serveStatic = require('serve-static');
+const serveStatic = require('serve-static');
 const app = express();
-const staticFileMiddleware = express.static(path.join(__dirname))
+// const staticFileMiddleware = express.static(path.join(__dirname))
 
-app.use(staticFileMiddleware)
-app.use(history())
-app.use(staticFileMiddleware)
-
-// app.get('/', function (req, res) {
-//     res.render(path.join(__dirname + '/index.html'))
-//   })
+// app.use(staticFileMiddleware)
+app.use(history({
+    verbose: true
+}))
+// app.use(staticFileMiddleware)
+app.use(serveStatic(__dirname + "/dist"));
+// app.use(__dirname + "/dist");
 
 var port = process.env.PORT || 5000;
 app.listen(port);
