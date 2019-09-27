5e9e74e189c3c5e7ddbb1cd1b9b567d5c64efb27
Paul Rector <paulre@skyward.com>
2019-09-27 13:54:48 -0500

    Updating server.js app.use

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 // server.js
 const history = require('connect-history-api-fallback')
-var express = require('express');
-var path = require('path');
-var serveStatic = require('serve-static');
+const express = require('express');
+const path = require('path');
+// var serveStatic = require('serve-static');
 const app = express();
+const staticFileMiddleware = express.static(path.join(__dirname))
 
-app.use(history({
-    verbose: true
-}))
-app.use(__dirname + "/dist");
+app.use(staticFileMiddleware)
+app.use(history())
+app.use(staticFileMiddleware)
+
+app.get('/', function (req, res) {
+    res.render(path.join(__dirname + '/index.html'))
+  })
+  
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+ port);
