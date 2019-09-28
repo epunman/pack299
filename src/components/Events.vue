9a039ebc5ef9acf2b7ed09d5005c54fe0b9f2037
Paul Rector <epunman@gmail.com>
2019-09-27 23:12:43 -0500

    remove comments

--- src/components/Events.vue
+++ src/components/Events.vue
@@ -7,54 +7,6 @@
             <li><a href="https://www.scoutbook.com/mobile/dashboard/calendar/event.asp?EventID=2439006" target="_blank" rel="noopener">Pack Meeting 9/17/19 6:30pm</a></li>
             <li><a href="https://www.scoutbook.com/mobile/dashboard/calendar/event.asp?EventID=2439203" target="_blank" rel="noopener">Webelos Fall Adventure 9/20/19-9/22/19</a></li>
             <li><a href="https://docs.google.com/document/d/1OXD9YnCUTupCrfO7rTAY_mS9K392yU1RqawAxZm4oVU/edit?usp=sharing" target="_blank" rel="noopener">SuperTrip to Mall of America Aquarium</a></li>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-            <li>4</li>
-            <li>5</li>
-            <li>6</li>
-            <li>7</li>
-            <li>8</li>
-            <li>9</li>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-            <li>4</li>
-            <li>5</li>
-            <li>6</li>
-            <li>7</li>
-            <li>8</li>
-            <li>9</li>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-            <li>4</li>
-            <li>5</li>
-            <li>6</li>
-            <li>7</li>
-            <li>8</li>
-            <li>9</li>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-            <li>4</li>
-            <li>5</li>
-            <li>6</li>
-            <li>7</li>
-            <li>8</li>
-            <li>9</li>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-            <li>4</li>
-            <li>5</li>
-            <li>5</li>
-            <li>6</li>
-            <li>7</li>
-            <li>8</li>
-            <li>9</li>
-            <li>1</li>
-            <li>2</li>
         </ul>
     </div>
 </template>
