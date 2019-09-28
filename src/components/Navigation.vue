b6246ae0b7f90f5f27642b6c50871ccb4ee0dd04
Paul Rector <epunman@gmail.com>
2019-09-27 23:11:59 -0500

    New logo

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,12 +1,12 @@
 <template>
     <div class="godown-60" id="godown">
     <b-navbar toggleable="lg" fixed="top" type="dark" variant="primary">
-        <b-navbar-brand to="/"><img alt="CSRanks" src="@/assets/logo.png" width="110" height="80"> Cub Scout Pack 299</b-navbar-brand>
+        <b-navbar-brand to="/"><img alt="CSRanks" src="@/assets/cubscoutlogo.png" width="50" height="48"> Cub Scout Pack 299</b-navbar-brand>
 
         <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
         <b-collapse id="nav-collapse" is-nav>
-        <b-navbar-nav>
+        <b-navbar-nav align-v="right">
             <b-nav-item to="events">Events</b-nav-item>
             <b-nav-item to="formalities" >Formalities</b-nav-item>
             <b-nav-item to="tools" >Tools</b-nav-item>
@@ -14,37 +14,24 @@
         </b-navbar-nav>
 
         <!-- Right aligned nav items -->
-        <b-navbar-nav class="ml-auto">
-            <!-- <b-nav-form>
+        <!-- <b-navbar-nav class="ml-auto">
+            <b-nav-form>
             <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
             <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
-            </b-nav-form> -->
+            </b-nav-form>
 
-            <b-nav-item-dropdown right>
+            <b-nav-item-dropdown right> -->
             <!-- Using 'button-content' slot -->
-            <template v-slot:button-content>
+            <!-- <template v-slot:button-content>
                 <em>User</em>
             </template>
             <b-dropdown-item href="#">Profile</b-dropdown-item>
             <b-dropdown-item href="#">Sign Out</b-dropdown-item>
             </b-nav-item-dropdown>
-        </b-navbar-nav>
+        </b-navbar-nav> -->
         </b-collapse>
     </b-navbar>
     </div>
-
-
-    <!-- <div class="navbar navbar-expand-lg navbar-dark bg-primary">
-        <nav class="collapse navbar-collapse" id="navbarNav">
-            <router-link to="/" class="navbar-brand"><img alt="CSRanks" src="@/assets/logo.png" width="110" height="80">   Cub Scout Pack 299</router-link>
-            <ul class="navbar-nav navbar-center">
-                <li class="nav-item"><router-link to="/events" class="nav-link">Events</router-link></li>
-                <li class="nav-item"><router-link to="/formalities" class="nav-link">Formalities</router-link></li>
-                <li class="nav-item"><router-link to="/tools" class="nav-link">Tools</router-link></li>
-                <li class="nav-item"><router-link to="/communication" class="nav-link">Communication</router-link></li>
-            </ul>
-        </nav>
-    </div> -->
 </template>
 
 <script>
