8b9e0823b56525f2bdb019f2c570491d6c264ea6
Paul Rector <paulre@skyward.com>
2019-10-09 16:40:59 -0500

    Updating event list

--- src/components/Events.vue
+++ src/components/Events.vue
@@ -4,9 +4,12 @@
     <div>
         <h3>Events</h3>
         <ul style="list-style: none;">
-            <li><a href="https://www.scoutbook.com/mobile/dashboard/calendar/event.asp?EventID=2439006" target="_blank" rel="noopener">Pack Meeting 9/17/19 6:30pm</a></li>
-            <li><a href="https://www.scoutbook.com/mobile/dashboard/calendar/event.asp?EventID=2439203" target="_blank" rel="noopener">Webelos Fall Adventure 9/20/19-9/22/19</a></li>
-            <li><a href="https://docs.google.com/document/d/1OXD9YnCUTupCrfO7rTAY_mS9K392yU1RqawAxZm4oVU/edit?usp=sharing" target="_blank" rel="noopener">SuperTrip to Mall of America Aquarium</a></li>
+            <li><a href="https://www.scoutbook.com/mobile/dashboard/calendar/event.asp?EventID=2579096" target="_blank" rel="noopener">Inaugural Central Wisconsin Indigenous Peoples’ Day Pow Wow 10/12/19 12:00pm</a></li>
+            <li><a href="https://www.scoutbook.com/mobile/dashboard/calendar/event.asp?EventID=2594928" target="_blank" rel="noopener">Lead pledge for school board meeting honoring a Washington teacher 10/14/19 6:15pm</a></li>
+            <li><a href="https://www.scoutbook.com/mobile/dashboard/calendar/event.asp?EventID=2538675" target="_blank" rel="noopener">Pack Meeting at the Stevens Point Police Department 10/15/19 6:30pm</a></li>
+            <li><a href="https://www.scoutbook.com/mobile/dashboard/calendar/event.asp?EventID=2581674" target="_blank" rel="noopener">Wildwood Zoo in Marshfield 11/2/19 12:00pm</a></li>
+            <li><a href="https://www.scoutbook.com/mobile/dashboard/calendar/event.asp?EventID=2581677" target="_blank" rel="noopener">CWES Overnight (indoor) camping 11/9/19-11/10/19</a></li>
+            <li><a href="https://www.scoutbook.com/mobile/dashboard/calendar/event.asp?EventID=2109260" target="_blank" rel="noopener">SuperTrip to Mall of America Aquarium</a></li>
         </ul>
     </div>
 </template>
