332a81429dc1b35d2308c8634b9d753da10f23b1
epunman <epunman@gmail.com>
2019-10-09 23:11:25 -0400

    Testing codeanywhere commits

--- src/components/NotFound.vue
+++ src/components/NotFound.vue
@@ -8,6 +8,6 @@
 
 <script>
 </script>
-<!-- Add "scoped" attribute to limit CSS to this component only -->
+<!-- Add "scoped" attribute to limit CSS to this component  only -->
 <style scoped>
 </style>
