33a77ca8656f0c60247d61e59c2114ce767b74cf
Paul Rector <paulre@skyward.com>
2019-10-14 15:32:18 -0500

    Update events list

--- src/components/Events.vue
+++ src/components/Events.vue
@@ -4,8 +4,6 @@
     <div>
         <h3>Events</h3>
         <ul style="list-style: none;">
-            <li><a href="https://www.scoutbook.com/mobile/dashboard/calendar/event.asp?EventID=2579096" target="_blank" rel="noopener">Inaugural Central Wisconsin Indigenous Peoples’ Day Pow Wow 10/12/19 12:00pm</a></li>
-            <li><a href="https://www.scoutbook.com/mobile/dashboard/calendar/event.asp?EventID=2594928" target="_blank" rel="noopener">Lead pledge for school board meeting honoring a Washington teacher 10/14/19 6:15pm</a></li>
             <li><a href="https://www.scoutbook.com/mobile/dashboard/calendar/event.asp?EventID=2538675" target="_blank" rel="noopener">Pack Meeting at the Stevens Point Police Department 10/15/19 6:30pm</a></li>
             <li><a href="https://www.scoutbook.com/mobile/dashboard/calendar/event.asp?EventID=2581674" target="_blank" rel="noopener">Wildwood Zoo in Marshfield 11/2/19 12:00pm</a></li>
             <li><a href="https://www.scoutbook.com/mobile/dashboard/calendar/event.asp?EventID=2581677" target="_blank" rel="noopener">CWES Overnight (indoor) camping 11/9/19-11/10/19</a></li>
